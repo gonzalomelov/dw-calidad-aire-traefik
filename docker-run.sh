280c329de5fce08b34661b41243b576ecf18e198
bieccamvd <admin@bieccamvd.com>
2019-11-10 14:11:36 -0600

    initial commit

---
+++ docker-run.sh
@@ -0,0 +1,11 @@
+docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $PWD/traefik.toml:/traefik.toml \
+  -v $PWD/acme.json:/acme.json \
+  -p 80:80 \
+  -p 443:443 \
+  -l traefik.frontend.rule=Host:monitor.bieccamvd.com \
+  -l traefik.port=8090 \
+  --network bridge \
+  --name traefik \
+  traefik:1.7.6-alpine
